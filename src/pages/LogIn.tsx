ba0fe92260e906894a69c88562d0e7fcbb81d04a
Stefano Schiavone <s.schiavone@student.fontys.nl>
2025-10-17 23:48:37 +0200

    feat: Create LogInCard

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -8,5 +8,17 @@ export default function Login() {
     navigate("/home");
   };
 
-  return <LogInCard onSubmit={handleSubmit} />;
+  return (
+    // TODO: Fix background gradient
+    <div className="relative min-h-screen flex items-center justify-center">
+      {/* Background gradient using ::before */}
+      <div
+        className="absolute inset-0 z-[-1] bg-[radial-gradient(ellipse_at_center,_hsl(210,100%,97%),_hsl(0,0%,100%))] 
+        dark:bg-[radial-gradient(ellipse_at_center,_hsla(210,100%,16%,0.5),_hsl(220,30%,5%))]"
+      ></div>
+
+      {/* Centered login card */}
+      <LogInCard onSubmit={handleSubmit} />
+    </div>
+  );
 }
